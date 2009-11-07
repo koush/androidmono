6d7fae7dbe9700846980e97f0cdc8f6267cdc89f
Koushik K. Dutta <koush@Koushik-MacBook.local>
2009-11-07 12:53:43 -0800

    Test app

---
+++ MonoTest/src/com/koushikdutta/monotest/MonoTestActivity.java
@@ -0,0 +1,13 @@
+package com.koushikdutta.monotest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MonoTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
