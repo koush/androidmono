ff1d2e2c37ca2436d9da0b2533cd1e762c9bcedf
Koushik K. Dutta <koush@Koushik-OSx86.local>
2009-12-02 19:32:04 -0800

    update the get-mono.sh script to handle incremental builds

--- get-mono.sh
+++ get-mono.sh
@@ -1,42 +1,69 @@
-pushd $(dirname $0)
-echo =====CLEARING OLD MONO BITS=====
+MONO_SVN_BASE=svn://anonsvn.mono-project.com/source/branches/mono-2-6
+MONO_SVN_REVISION=147526
 
+pushd $(dirname $0)
+
+function check-result
+{
+    RESULT=`echo $?`
+    if [ "$RESULT" != 0 ]; then
+        echo $1
+        exit $RESULT
+    fi
+}
+
+echo
+echo =====CHECKING OUT MONO=====
 if [ -d mono ]
 then
-  rm -rf mono
+    echo =====UPDATING EXISTING MONO CHECKOUT=====
+    echo =====If you get errors, you may want to delete mono and hostbuild to force a clean build=====
+    pushd mono
+    MONO_SKIP_PATCH=true
+    svn up -r $MONO_SVN_REVISION
+    popd
+    check-result 'Error while updating ./mono'
+    pushd hostbuild/mono
+    svn up -r $MONO_SVN_REVISION
+    popd
+    check-result 'Error while updating ./hostbuild/mono'
+    pushd hostbuild/mcs
+    svn up -r $MONO_SVN_REVISION
+    popd
+    check-result 'Error while updating ./hostbuild/mcs'
+else
+    svn co -r $MONO_SVN_REVISION $MONO_SVN_BASE/mono
+    mkdir -p hostbuild
+    cp -r mono hostbuild/mono
+    pushd hostbuild
+    svn co -r $MONO_SVN_REVISION $MONO_SVN_BASE/mcs
+    popd
 fi
 
-if [ -d hostbuild ]
-then
-  rm -rf hostbuild
-fi
-
-MONO_SVN_BASE=svn://anonsvn.mono-project.com/source/branches/mono-2-6
-MONO_SVN_REVISION=147505
-
 echo
-echo =====CHECKING OUT MONO AND MCS=====
-svn co -r $MONO_SVN_REVISION $MONO_SVN_BASE/mono
-mkdir -p hostbuild
-cp -r mono hostbuild/mono
-pushd hostbuild
-svn co -r $MONO_SVN_REVISION $MONO_SVN_BASE/mcs
-
-pushd mono/eglib
-echo
-echo =====COMPILING MONO CLASS LIBRARIES FOR HOST BUILD=====
+echo =====COMPILING MONO CLASS LIBRARIES FOR MCS BUILD=====
 sleep 2
-./autogen.sh
+pushd hostbuild/mono/eglib
+if [ ! -f configure ]
+then
+    ./autogen.sh
+fi
 popd
-cd mono
-./autogen.sh --with-glib=embedded --with-moonlight=no
+pushd hostbuild/mono
+if [ ! -f configure ]
+then
+    ./autogen.sh --with-glib=embedded --with-moonlight=no
+fi
 make
 popd
 
-
-cd mono
+pushd mono
 echo
-echo =====APPLYING ANDROID PATCHES FOR TARGET BUILD=====
-patch -p0 -i ../mono.patch
-
+echo =====APPLYING ANDROID PATCHES FOR ARM BUILD=====
+if [ -z $MONO_SKIP_PATCH ]
+then
+    patch -p0 -i ../mono.patch
+fi
 popd
+
+echo "=====Done! Don't forget Rebuild the ARM build using the Android NDK now!====="
