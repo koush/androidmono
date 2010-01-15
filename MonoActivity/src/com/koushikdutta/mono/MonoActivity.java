e1157c6dad2b581ca7ae4024dba0344e821c7186
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-01-14 22:35:55 -0800

    fix mono activity

--- MonoActivity/src/com/koushikdutta/mono/MonoActivity.java
+++ MonoActivity/src/com/koushikdutta/mono/MonoActivity.java
@@ -10,9 +10,6 @@ import java.util.Vector;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-import com.koushikdutta.monodalvikbridge.MonoBridge;
-import com.koushikdutta.monodalvikbridge.MonoObject;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -160,52 +157,5 @@ public class MonoActivity extends Activity
 			}
 		}
 		return list;
-	}
-
-	MenuItem mTestItem;
-	MenuItem mInvokeItem;
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		mTestItem = menu.add("Test");
-		mInvokeItem = menu.add("Invoke");
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item)
-	{
-		if (item == mTestItem)
-		{
-			MonoBridge.Initialize();
-		}
-		else if (item == mInvokeItem)
-		{
-			try
-			{
-				//String ret = (String)MonoBridge.invoke("MonoDalvikBridge.DalvikBridge, MonoDalvikBridge", String.class, "Echo", "hello");
-				//Log.i(LOGTAG, String.format("MonoActivity %s", ret == null ? "null" : ret));
-				
-				//MonoObject one = MonoBridge.newMonoObject("MonoDalvikBridge.TestObject, MonoDalvikBridge");
-				//MonoObject two = MonoBridge.newMonoObject("MonoDalvikBridge.TestObject, MonoDalvikBridge", "Test");
-				
-				//one.invoke("Foo");
-				//two.invoke("Foo");
-				
-				MonoObject args = MonoBridge.newMonoObject("System.Object[]", 1); 
-				byte[] runtimeTypes = new byte[2];
-				runtimeTypes[0] = MonoBridge.String;
-				runtimeTypes[1] = MonoBridge.Int32;
-				//MonoObject javaClass = (MonoObject)MonoBridge.invoke("System.String", MonoObject.class, "Intern", new Object[] { "com.koushikdutta.monodalvikbridge.MonoBridge" });
-				//MonoObject javaMethod = (MonoObject)MonoBridge.invoke("System.String", MonoObject.class, "Intern", new Object[] { "echo" });
-				args.invoke(void.class, "SetValue", new Object[] { "boof", 0 }, runtimeTypes);
-				MonoBridge.invoke("MonoDalvikBridge.DalvikBridge, MonoDalvikBridge", "Invoke", "com.koushikdutta.monodalvikbridge.MonoBridge", "echo", args);
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		return super.onMenuItemSelected(featureId, item);
 	}
 }
